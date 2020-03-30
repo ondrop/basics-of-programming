89c507da59af85b076cb6192c9148268727d49c6
ondrop <61581458+ondrop@users.noreply.github.com>
2020-03-31 01:01:07 +0300

    Update

--- Lw18/181.pas
+++ Lw18/181.pas
@@ -1,41 +1,45 @@
 PROGRAM AverageScore(INPUT, OUTPUT);
 CONST
-  NumberOfScores = 3;
-  ClassSize = 5;
+  NumberOfScores = 4;
+  ClassSize = 4;
 TYPE
   Score = 0 .. 100;
 VAR
-  WhichScore: 1 .. NumberOfScores;
-  Student: 1 .. ClassSize;
+  WhichScore: 0 .. NumberOfScores;
+  Student: 0 .. ClassSize;
   NextScore: Score;
   Ave, TotalScore, ClassTotal: INTEGER;
 BEGIN {AverageScore}
   ClassTotal := 0;
   WRITELN('Student averages:');
-  Student := 1;
-  WHILE Student < ClassSize
-  DO 
-    BEGIN
+  Student := 0;              
+  WHILE (Student < ClassSize)
+  DO                                                                         
+    BEGIN                    
+      Student := Student + 1;
       TotalScore := 0;
-      WhichScore := 1;
-      WHILE WhichScore < 3
-      DO
-        BEGIN
+      WhichScore := 0;      
+      WRITELN('Enter grades of ', Student, ' student.');
+      WHILE (WhichScore < 4) AND (NOT(EOLN)) 
+      DO                                                                                
+        BEGIN              
           READ(NextScore);
-          TotalScore := TotalScore + NextScore
+          TotalScore := TotalScore + NextScore;
+          WhichScore := WhichScore + 1
         END;
       READLN;
       TotalScore := TotalScore * 10;
       Ave := TotalScore DIV NumberOfScores;
-      IF Ave MOD 10 >= 5
+      WRITE('Average score of student ', Student, ': ');                              
+      IF (Ave MOD 10) >= 5
       THEN
-        WRITE(Ave DIV 10 + 1)
+        WRITELN(Ave DIV 10 + 1)
       ELSE
-        WRITE(Ave DIV 10);
+        WRITELN(Ave DIV 10);
       ClassTotal := ClassTotal + TotalScore
     END;
   WRITELN;
-  WRITELN ('Class average:');
-  ClassTotal := ClassTotal DIV (ClassSize *NumberOfScores);
-  WRITELN(ClassTotal DIV 10, '.' ClassTotal MOD 10:1)
+  WRITELN('Class average:');
+  ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
+  WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
 END.  {AverageScore}
