cc45b9082a2b15233237ecdb7388e5fa5380aa2c
ondrop <61581458+ondrop@users.noreply.github.com>
2020-03-30 22:59:55 +0300

    Upload

---
+++ Lw17/171.pas
@@ -0,0 +1,37 @@
+PROGRAM TestReadDigit(INPUT, OUTPUT);
+VAR
+  Digit, Sum: INTEGER;
+PROCEDURE ReadDigit(VAR InF: TEXT; VAR D: INTEGER);
+{Считывает текущий символ из файла, если он - цифра, возвращает его 
+ преобразуя в значение типа INTEGER. Если считанный символ не цифра
+ возвращает -1}
+VAR
+  Ch: CHAR;
+  Error: INTEGER; 
+BEGIN {ReadDigit}
+  IF NOT(EOLN(InF))
+  THEN
+    BEGIN
+      READ(InF, Ch);
+      IF (Ch >= '0') AND (Ch <= '9')
+      THEN
+        {Процедура Val, преобразовывает строковое значение 
+         в его числовое представление}
+        Val(Ch, D, Error)
+      ELSE
+        D := -1 
+    END 
+END; {ReadDigit} 
+BEGIN {TestReadDigit}
+  Digit := 0;
+  Sum := 0;
+  WHILE NOT(EOLN(INPUT)) AND (Digit <> -1) 
+  DO
+    BEGIN
+      ReadDigit(INPUT, Digit);
+      IF Digit <> -1
+      THEN
+        Sum := Sum + Digit
+    END;
+  WRITELN(OUTPUT, Sum)
+END. {TestReadDigit}
---
+++ Lw17/172.pas
@@ -0,0 +1,36 @@
+PROGRAM TestReadNumber(INPUT, OUTPUT);
+VAR
+  Number: INTEGER;
+PROCEDURE ReadNumber(VAR InF: TEXT; VAR N: INTEGER);
+{Преобразует строку цифр из файла до первого нецифрового
+ символа,  в соответствующее целое число N}
+VAR
+  Ch: CHAR;
+  D, Error: INTEGER; 
+BEGIN {ReadNumber}
+  Ch := '0';
+  N := 0; 
+  WHILE (NOT(EOLN(InF))) AND ((Ch >= '0') AND (Ch <= '9')) AND (N <> -1)
+  DO
+    BEGIN
+      READ(InF, Ch);
+      IF (Ch >= '0') AND (Ch <= '9')
+      THEN
+        BEGIN
+          Val(Ch, D, Error);
+          IF (N < 3276)
+          THEN
+            N := (N * 10) + D
+          ELSE
+            IF (Ch <= '7') AND (N = 3276)
+            THEN
+              N := (N * 10) + D
+            ELSE
+              N := -1    
+        END
+    END
+END; {ReadNumber} 
+BEGIN {TestReadNumber}
+  ReadNumber(INPUT, Number);
+  WRITELN(OUTPUT, Number)
+END. {TestReadNumber}
