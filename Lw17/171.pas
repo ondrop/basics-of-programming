6d8cc3c75200aecb7772b6d68f491e5327d5edc5
ondrop <61581458+ondrop@users.noreply.github.com>
2020-03-31 23:53:15 +0300

    Update

--- Lw17/171.pas
+++ Lw17/171.pas
@@ -4,23 +4,28 @@ VAR
 PROCEDURE ReadDigit(VAR InF: TEXT; VAR D: INTEGER);
 VAR
   Ch: CHAR;
-  Error: INTEGER; 
 BEGIN {ReadDigit}
+  D := -1; 
   IF NOT(EOLN(InF))
   THEN
     BEGIN
       READ(InF, Ch);
-      IF (Ch >= '0') AND (Ch <= '9')
-      THEN
-        Val(Ch, D, Error)
-      ELSE
-        D := -1 
+      IF (Ch = '0') THEN D := 0 ELSE
+      IF (Ch = '1') THEN D := 1 ELSE
+      IF (Ch = '2') THEN D := 2 ELSE
+      IF (Ch = '3') THEN D := 3 ELSE
+      IF (Ch = '4') THEN D := 4 ELSE
+      IF (Ch = '5') THEN D := 5 ELSE
+      IF (Ch = '6') THEN D := 6 ELSE
+      IF (Ch = '7') THEN D := 7 ELSE
+      IF (Ch = '8') THEN D := 8 ELSE
+      IF (Ch = '9') THEN D := 9 
     END 
 END; {ReadDigit} 
 BEGIN {TestReadDigit}
   Digit := 0;
   Sum := 0;
-  WHILE NOT(EOLN(INPUT)) AND (Digit <> -1) 
+  WHILE (Digit <> -1) 
   DO
     BEGIN
       ReadDigit(INPUT, Digit);
--- Lw17/172.pas
+++ Lw17/172.pas
@@ -4,28 +4,35 @@ VAR
 PROCEDURE ReadNumber(VAR InF: TEXT; VAR N: INTEGER); 
 VAR
   Ch: CHAR;
-  D, Error: INTEGER; 
+  D: INTEGER; 
 BEGIN {ReadNumber}
   Ch := '0';
   N := 0; 
   WHILE (NOT(EOLN(InF))) AND ((Ch >= '0') AND (Ch <= '9')) AND (N <> -1)
   DO
-    BEGIN
+    BEGIN 
+      D := -1;
       READ(InF, Ch);
-      IF (Ch >= '0') AND (Ch <= '9')
+      IF (Ch = '0') THEN D := 0 ELSE
+      IF (Ch = '1') THEN D := 1 ELSE
+      IF (Ch = '2') THEN D := 2 ELSE
+      IF (Ch = '3') THEN D := 3 ELSE
+      IF (Ch = '4') THEN D := 4 ELSE                             
+      IF (Ch = '5') THEN D := 5 ELSE
+      IF (Ch = '6') THEN D := 6 ELSE
+      IF (Ch = '7') THEN D := 7 ELSE
+      IF (Ch = '8') THEN D := 8 ELSE
+      IF (Ch = '9') THEN D := 9; 
+      IF (D <> -1)
       THEN
         BEGIN
-          Val(Ch, D, Error);
-          IF (N < 3276)
+          N := N * 10;
+          IF (D <= (MAXINT - N))
           THEN
-            N := (N * 10) + D
+            N := N + D
           ELSE
-            IF (Ch <= '7') AND (N = 3276)
-            THEN
-              N := (N * 10) + D
-            ELSE
-              N := -1    
-        END
+            N := -1    
+        END     
     END
 END; {ReadNumber} 
 BEGIN {TestReadNumber}
