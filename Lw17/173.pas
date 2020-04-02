aa9a74ddd30c2bb23ab4de74a9dbcf965db4fd99
ondrop <61581458+ondrop@users.noreply.github.com>
2020-04-02 11:32:25 +0300

    Update

--- Lw17/173.pas
+++ Lw17/173.pas
@@ -1,90 +1,133 @@
 PROGRAM Stat(INPUT, OUTPUT);
 VAR
-  Average, Num, NumMax, NumMin, Counter, State: INTEGER;
-PROCEDURE ReadNumber(VAR InF: TEXT; VAR N, State: INTEGER);
+  FileForStat: TEXT;
+  Ch: CHAR;
+  Average, Num, NumMax, NumMin, Counter, Overflow, InvalidData: INTEGER;
+PROCEDURE ReadNumber(VAR InF: TEXT; VAR N: INTEGER); 
 VAR
   Ch: CHAR;
-  D, Error: INTEGER; 
+  D: INTEGER; 
 BEGIN {ReadNumber}
   Ch := '0';
-  N := 0;
-  State := 0; 
+  N := 0; 
   WHILE (NOT(EOLN(InF))) AND ((Ch >= '0') AND (Ch <= '9')) AND (N <> -1)
+  DO
+    BEGIN 
+      D := -1;
+      READ(InF, Ch);
+      IF (Ch = '0') THEN D := 0 ELSE
+      IF (Ch = '1') THEN D := 1 ELSE
+      IF (Ch = '2') THEN D := 2 ELSE
+      IF (Ch = '3') THEN D := 3 ELSE
+      IF (Ch = '4') THEN D := 4 ELSE                             
+      IF (Ch = '5') THEN D := 5 ELSE
+      IF (Ch = '6') THEN D := 6 ELSE
+      IF (Ch = '7') THEN D := 7 ELSE
+      IF (Ch = '8') THEN D := 8 ELSE
+      IF (Ch = '9') THEN D := 9; 
+      IF (D <> -1)
+      THEN
+        BEGIN
+          N := N * 10;
+          IF (D <= (32767 - N))
+          THEN
+            N := N + D
+          ELSE
+            N := -1    
+        END
+      ELSE
+        IF Ch <> ' '
+        THEN
+          N := -2       
+    END
+END; {ReadNumber}
+PROCEDURE Copy(VAR InF, OutF: TEXT);
+VAR
+  Ch: CHAR;
+BEGIN {Copy}
+  WHILE NOT(EOLN(InF))
   DO
     BEGIN
       READ(InF, Ch);
-      IF (Ch >= '0') AND (Ch <= '9')
+      WRITE(OutF, Ch)
+    END;
+  WRITELN(OutF)
+END; {Copy} 
+PROCEDURE CheckSpace(VAR InF: TEXT; VAR N: INTEGER);
+VAR
+  Ch, W1, W2: CHAR;
+BEGIN {DelSpace}
+  RESET(InF);
+  W1 := '0';
+  W2 := '0';
+  IF NOT(EOLN(InF))
+  THEN
+    BEGIN
+      READ(InF, Ch);  
+      IF Ch = ' '
       THEN
-        BEGIN
-          Val(Ch, D, Error);
-          IF (N < 3276)
-          THEN
-            BEGIN
-              N := (N * 10) + D;
-              State := 1
-            END
-          ELSE
-            IF (Ch <= '7') AND (N = 3276)                                                           
-            THEN
-              BEGIN
-                N := (N * 10) + D;
-                State := 1
-              END
-            ELSE
-              BEGIN
-                N := -1;
-                Ch := '0';
-                WHILE (NOT(EOLN(InF)) AND (Ch = '0'))
-                DO
-                  READ(InF, Ch)
-              END    
-        END
-    END  
-END; {ReadNumber} 
-BEGIN {Stat} 
-  Num := -1;
-  NumMax := 0;
-  NumMin := 0;
+        N := -2
+      ELSE
+        RESET(InF);
+      WHILE ((W1 <> ' ') OR (W2 <> ' ')) AND (NOT(EOLN(InF)))
+      DO
+        BEGIN         
+          W1 := W2;
+          READ(InF, W2)
+        END;
+      IF (W1 = ' ') AND (W2 = ' ')
+      THEN
+        N := -2
+      ELSE      
+        RESET(InF)          
+    END;  
+END; {DelSpace}
+BEGIN {Stat}
+  Num := 0;
+  NumMax := -1;
+  NumMin := -1;
   Counter := 0;
   Average := 0;
-  WHILE ((Num = -1) OR (State = 0)) AND (NOT(EOLN(INPUT)))
+  Overflow := 0;
+  InvalidData := 0;    
+  REWRITE(FileForStat);
+  Copy(INPUT, FileForStat);
+  CheckSpace(FileForStat, Num);
+  WHILE (NOT(EOLN(FileForStat))) AND (Overflow = 0) AND (InvalidData = 0) AND (Num <> -2)
   DO
     BEGIN
-      ReadNumber(INPUT, Num, State);
-      IF ((Num <> -1) AND (State <> 0)) 
+      ReadNumber(FileForStat, Num);
+      IF (Num <> -1) AND (Num <> -2) AND ((32767 - Average) >= Num)
       THEN
         BEGIN
-          NumMax := Num;
-          NumMin := Num;          
-          Counter := Counter + 1; 
-          WRITELN(OUTPUT, Counter);
-          Average := Average + Num
-        END                                 
-    END; 
-  WHILE NOT(EOLN(INPUT))
-  DO
-    BEGIN
-      ReadNumber(INPUT, Num, State);
-      IF ((Num <> -1) AND (State <> 0)) AND ((32767 - Average) > Num)
-      THEN
-        BEGIN
-          Average := Average + Num;                                    
-          Counter := Counter + 1;  
-          IF (NumMax <= Num)
+          Counter := Counter + 1;
+          WRITELN(OUTPUT, 'TEST ', Counter);
+          Average := Average + Num;
+          IF (NumMax <= Num) OR (NumMax = -1)
           THEN
-            NumMax := Num
-          ELSE  
-           IF (NumMin >= Num)
-           THEN
-             NumMin := Num
-        END    
-    END;
-  WRITELN(OUTPUT, 'Maximum number = ', NumMax);  
-  WRITELN(OUTPUT, 'Minimum number = ', NumMin);
-  WRITE(OUTPUT, 'Average score = ');
-  IF (Counter <> 0)
-  THEN  
-    WRITELN(OUTPUT, Average DIV Counter, '.', (Average MOD Counter) * 100 DIV Counter)
+            NumMax := Num; 
+          IF (NumMin >= Num) OR (NumMin = -1)
+          THEN
+            NumMin := Num
+        END
+      ELSE    
+        IF (Num = -2) 
+        THEN 
+          InvalidData := 1
+        ELSE 
+         Overflow := 1
+    END;     
+  IF (Counter <> 0) AND (Overflow = 0) AND (InvalidData = 0)
+  THEN
+    BEGIN
+      WRITELN(OUTPUT, 'Maximum number = ', NumMax);  
+      WRITELN(OUTPUT, 'Minimum number = ', NumMin);
+      WRITELN(OUTPUT, 'Average score = ', Average DIV Counter, '.', (Average MOD Counter) * 100 DIV Counter)
+    END
   ELSE
-    WRITELN(OUTPUT, Average)  
+    IF Overflow = 1
+    THEN
+      WRITELN(OUTPUT, 'Overflow')
+    ELSE
+      WRITELN(OUTPUT, 'Invalid data')      
 END. {Stat}
