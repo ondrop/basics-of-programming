c4a4ec08e1f119b2af2987551aa445cede219621
ondrop <61581458+ondrop@users.noreply.github.com>
2020-04-21 22:06:18 +0300

    Fixed

--- Lw19/191.pas
+++ Lw19/191.pas
@@ -5,26 +5,25 @@ CONST
 VAR
   Sieve: SET OF Min .. Max;
   NextNumber, TotalNumber: INTEGER;
-  Ch: CHAR;
 BEGIN {Prime}    
   Sieve := [Min .. Max];   
   NextNumber := Min;
   TotalNumber := Min;
-  WRITE(OUTPUT, 'Простые числа в диапазоне до ', Max, ' будут ');
-  WHILE (NextNumber <= Max)
+  WRITE('Простые числа в диапазоне до ', Max, ' будут ');
+  WHILE NextNumber <= Max
   DO
     BEGIN                       
-      WHILE (TotalNumber <= Max) 
+      WHILE TotalNumber <= Max 
       DO
         BEGIN
           Sieve := Sieve - [TotalNumber];
-          TotalNumber := TotalNumber + NextNumber;                
+          TotalNumber := TotalNumber + NextNumber               
         END;    
       WRITE(NextNumber, ' ');    
-      WHILE (NOT(NextNumber IN Sieve)) AND (NextNumber <= Max)  
+      WHILE NOT(NextNumber IN Sieve) AND (NextNumber <= Max)  
       DO
         NextNumber := NextNumber + 1;
       TotalNumber := NextNumber
     END;         
-  WRITELN(OUTPUT)     
-END. {Prime}                        
+  WRITELN     
+END. {Prime}                
